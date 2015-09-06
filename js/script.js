6458c324b3d4c5bc1fba2a8c1378111cacd763c3
Jayendra <jay08jog@gmail.com>
2015-09-05 20:34:36 -0700

    more updates

--- js/script.js
+++ js/script.js
@@ -16,64 +16,18 @@ window.onload = function(){
         ]
     };
     var options = {
-    //Boolean - Whether to show lines for each scale point
-    scaleShowLine : true,
-
-    //Boolean - Whether we show the angle lines out of the radar
-    angleShowLineOut : true,
-
-    //Boolean - Whether to show labels on the scale
-    scaleShowLabels : false,
-
-    // Boolean - Whether the scale should begin at zero
-    scaleBeginAtZero : true,
-
-    //String - Colour of the angle line
     angleLineColor : "white",
-
-    //Number - Pixel width of the angle line
-    angleLineWidth : 1,
-
-    //String - Point label font declaration
-    pointLabelFontFamily : "'Arial'",
-
-    //String - Point label font weight
-    pointLabelFontStyle : "normal",
-
-    //Number - Point label font size in pixels
     pointLabelFontSize : 15,
-
-    //String - Point label font colour
     pointLabelFontColor : "white",
-
-    //Boolean - Whether to show a dot for each point
-    pointDot : true,
-
-    //Number - Radius of each point dot in pixels
-    pointDotRadius : 3,
-
-    //Number - Pixel width of point dot stroke
-    pointDotStrokeWidth : 1,
-
-    //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
-    pointHitDetectionRadius : 20,
-
-    //Boolean - Whether to show a stroke for datasets
-    datasetStroke : true,
-
-    //Number - Pixel width of dataset stroke
-    datasetStrokeWidth : 2,
-
-    //Boolean - Whether to fill the dataset with a colour
-    datasetFill : true,
-
-    //String - A legend template
-    legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>",
-
     scaleLineColor : "white",
-
     responsive: true
-
-    }
+    };
     new Chart(ctx).Radar(data, options);
 };
+
+$(function(){
+        $(".element").typed({
+            strings: ["Hello World!", "My name is Jay", "I'm currently studying Computer Science at UCLA", "Jayendra Jog"],
+            typeSpeed: 0
+        });
+    });
